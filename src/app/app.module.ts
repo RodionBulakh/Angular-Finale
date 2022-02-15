11a9b750100734d9a6d3f5a7ec2ec13be90ec877
Rodion Bulakh <bulakhrodion1@gmail.com>
2022-02-15 14:08:35 +0200

    basic structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/general/menu/menu.component';
+import { LoginFormComponent } from './components/general/login-form/login-form.component';
+import { GamesComponent } from './components/pages/games/games.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
+import { LibraryComponent } from './components/pages/library/library.component';
+import { FriendsComponent } from './components/pages/friends/friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginFormComponent,
+    GamesComponent,
+    ProfileComponent,
+    LibraryComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
