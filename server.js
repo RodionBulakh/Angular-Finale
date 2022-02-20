69f44708240ac3752cee0877ee5e64cfec570d1c
Rodion Bulakh <bulakhrodion1@gmail.com>
2022-02-20 22:30:23 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/angular-finale')));
+app.use(express.static(__dirname + '/dist/angular-finale'));
 
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root: 'dist/angular-finale/' });
